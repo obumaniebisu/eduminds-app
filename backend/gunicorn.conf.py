6654875d7d0093961fc391d1f52b13a460c6043b
Obuma Ni-ebisu <your-email@example.com>
2025-12-29 06:45:20 +0100

    Update gunicorn config for Azure

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,9 +1,12 @@
 import multiprocessing
 
-max_requests = 1000
-max_requests_jitter = 50
-log_file = "-"
-bind = "0.0.0.0:8000"
-timeout = 230
+bind = "0.0.0.0:8000"   # Azure requires this
 workers = (multiprocessing.cpu_count() * 2) + 1
 worker_class = "uvicorn.workers.UvicornWorker"
+
+max_requests = 1000
+max_requests_jitter = 50
+timeout = 230
+loglevel = "info"
+accesslog = "-"
+errorlog = "-"
